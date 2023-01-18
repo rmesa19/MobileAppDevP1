e24b76919d39fd83c2c77eee2756935045ef60fe
Rudy <rmesa19@gmail.com>
2023-01-18 11:06:06 -0500

    created build

--- app/src/main/java/com/example/numad23sp_rodolphmesadieu/MainActivity.java
+++ app/src/main/java/com/example/numad23sp_rodolphmesadieu/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.numad23sp_rodolphmesadieu;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button);
+
+        btn1.setOnClickListener(view -> Toast.makeText(MainActivity.this, "Rodolph Mesadieu\nmesadieu.r@northeastern.edu",
+                Toast.LENGTH_SHORT).show());
+
     }
 }
