539a4b68b500c5670a79f540056da2daa4a3c507
Rudy <rmesa19@gmail.com>
2023-01-29 21:43:47 -0500

    added new activity

--- app/src/main/java/com/example/numad23sp_rodolphmesadieu/MainActivity.java
+++ app/src/main/java/com/example/numad23sp_rodolphmesadieu/MainActivity.java
@@ -7,13 +7,13 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1;
+
+    Button btn1 = findViewById(R.id.button);
+    Button btn2 = findViewById(R.id.button2);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn1 = findViewById(R.id.button);
 
         btn1.setOnClickListener(view -> Toast.makeText(MainActivity.this, "Rodolph Mesadieu\nmesadieu.r@northeastern.edu",
                 Toast.LENGTH_SHORT).show());
---
+++ app/src/main/java/com/example/numad23sp_rodolphmesadieu/NewActivity.java
@@ -0,0 +1,14 @@
+package com.example.numad23sp_rodolphmesadieu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
